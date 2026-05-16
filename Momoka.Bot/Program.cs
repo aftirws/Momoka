f75681dd41fc90d632cb6ee20a03405cce351408
Mati <33140484-Mxtisiek@users.noreply.gitlab.com>
2026-05-17 00:05:09 +0200

    Intents

--- Momoka.Bot/Program.cs
+++ Momoka.Bot/Program.cs
@@ -1,1 +1,17 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.Hosting;
+
+using NetCord.Gateway;
+using NetCord.Hosting.Gateway;
+
+var builder = Host.CreateApplicationBuilder(args);
+
+builder.Services
+    .AddDiscordGateway(options =>
+    {
+        options.Intents = GatewayIntents.GuildMessages | GatewayIntents.DirectMessages | GatewayIntents.MessageContent;
+    })
+    .AddGatewayHandlers(typeof(Program).Assembly);
+
+var host = builder.Build();
+
+await host.RunAsync();
